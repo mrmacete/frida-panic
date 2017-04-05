27e772cda1644d9505c7c47e05f1702608bd90eb
mrmacete <mrmacete@protonmail.ch>
2017-04-05 18:26:02 +0200

    Add raw stack dump, c++ throw_system_error exception

--- index.js
+++ index.js
@@ -11,8 +11,11 @@ module.exports = {
 
       if (Process.platform === 'darwin') {
         const objcThrow = Module.findExportByName('libobjc.A.dylib', 'objc_exception_throw');
+        let potentialObjCPanic = null;
+        const cppThrowSystemError = Module.findExportByName('libc++.1.dylib', '_ZNSt3__120__throw_system_errorEiPKc');
+        let potentialCPPPanic = null;
+
         if (objcThrow !== null) {
-          let potentialObjCPanic = null;
 
           Interceptor.attach(objcThrow, function (args) {
             const exception = new ObjC.Object(args[0]);
@@ -22,18 +25,44 @@ module.exports = {
               details: preparePanic(`Unhandled Objective-C exception: ${description}`, {}, this.context)
             };
           });
+        }
+
+        if (cppThrowSystemError !== null) {
+
+          Interceptor.attach(cppThrowSystemError, function (args) {
+            const description = args[1].isNull() ? '' : Memory.readUtf8String(args[1]);
+            potentialCPPPanic = {
+              description: description,
+              details: preparePanic(`Unhandled C++ exception: ${description}`, {}, this.context)
+            };
+          });
+        }
+
+        if (objcThrow !== null || cppThrowSystemError !== null) {
 
           Interceptor.attach(Module.findExportByName('libsystem_c.dylib', 'abort'), {
             onEnter(args) {
-              const isCausedByUnhandledObjCException = Thread.backtrace(this.context).map(DebugSymbol.fromAddress).some(symbol => {
+              const symbols = Thread.backtrace(this.context).map(DebugSymbol.fromAddress);
+              const isCausedByUnhandledObjCException = symbols.some(symbol => {
                 return symbol.moduleName === 'libobjc.A.dylib' && symbol.name === '_objc_terminate()';
+              });
+              const isCausedByCPPException = symbols.some(symbol => {
+                return symbol.moduleName === 'libc++.1.dylib' && symbol.name === 'std::__1::__throw_system_error(int, char const*)';
               });
               if (isCausedByUnhandledObjCException) {
                 const details = unsafeOperations[Process.getCurrentThreadId()];
                 if (details !== undefined) {
                   details.exception = new Error(potentialObjCPanic.description);
-                } else {
+                } else if (potentialObjCPanic) {
                   sink.onPanic(potentialObjCPanic.details);
+                }
+              }
+              if (isCausedByCPPException) {
+                const details = unsafeOperations[Process.getCurrentThreadId()];
+                if (details !== undefined) {
+                  details.exception = new Error(potentialCPPPanic.description);
+                } else if (potentialCPPPanic) {
+                  sink.onPanic(potentialCPPPanic.details);
                 }
               }
             }
@@ -49,9 +78,26 @@ module.exports = {
           details: details,
           stack: {
             native: backtrace.map(frame => frame.toString()).join('\n'),
-            js: new Error().stack
+            js: new Error().stack,
+            raw: digStack(cpuContext).join('\n')
           }
         };
+      }
+
+      function digStack(cpuContext) {
+        const manualTrace = [];
+
+        for (let i = 0; i !== 128; i++) {
+          try {
+            const addr = Memory.readPointer(cpuContext.sp.add(i * Process.pointerSize));
+            manualTrace.push(
+              DebugSymbol.fromAddress(addr).toString()
+            );
+          } catch(e) {
+          }
+        }
+
+        return manualTrace;
       }
     }
   },
@@ -83,6 +129,9 @@ ${'\t' + error.stack.native.replace(/\n/g, '\n\t')}
 
 JavaScript stack:
 ${'\t' + error.stack.js.replace(/\n/g, '\n\t')}
+
+Raw stack:
+${'\t' + error.stack.raw.replace(/\n/g, '\n\t')}
 
 Details:
 ${'\t' + JSON.stringify(error.details, null, 4).replace(/\n/g, '\n\t')}
